1ae68cc5ba26a6056a0d8b62608e4fdb1e3f6073
Julien Dubois <julien.dubois@gmail.com>
2021-04-02 16:07:14 +0200

    Update Gradle code to the latest version

--- src/main/java/com/example/HelloHandler.java
+++ src/main/java/com/example/HelloHandler.java
@@ -16,11 +16,15 @@ public class HelloHandler extends AzureSpringBootRequestHandler<User, Greeting> 
     public HttpResponseMessage execute(
             @HttpTrigger(name = "request", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<User>> request,
             ExecutionContext context) {
-
-        context.getLogger().info("Greeting user name: " + request.getBody().get().getName());
+        User user = request.getBody()
+                .filter((u -> u.getName() != null))
+                .orElseGet(() -> new User(
+                        request.getQueryParameters()
+                                .getOrDefault("name", "world")));
+        context.getLogger().info("Greeting user name: " + user.getName());
         return request
                 .createResponseBuilder(HttpStatus.OK)
-                .body(handleRequest(request.getBody().get(), context))
+                .body(handleRequest(user, context))
                 .header("Content-Type", "application/json")
                 .build();
     }
--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -2,14 +2,14 @@ package com.example.model;
 
 public class User {
 
+    private String name;
+
     public User() {
     }
 
     public User(String name) {
         this.name = name;
     }
-
-    private String name;
 
     public String getName() {
         return name;
