546861ec722a4e216f7ac1d8cff27ed1e2ca9b41
Julien Dubois <julien.dubois@gmail.com>
2021-05-25 14:47:34 +0200

    Refactor function name

--- src/test/java/com/example/HelloFunctionTest.java
+++ src/test/java/com/example/HelloFunctionTest.java
@@ -4,7 +4,6 @@ import com.example.model.Greeting;
 import com.example.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.cloud.function.adapter.azure.AzureSpringBootRequestHandler;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,14 +12,14 @@ public class HelloFunctionTest {
 
     @Test
     public void test() {
-        Mono<Greeting> result = new HelloFunction().apply(Mono.just(new User("foo")));
+        Mono<Greeting> result = new Hello().apply(Mono.just(new User("foo")));
         assertThat(result.block().getMessage()).isEqualTo("Hello, foo!\n");
     }
 
     @Test
     public void start() {
         AzureSpringBootRequestHandler<User, Greeting> handler = new AzureSpringBootRequestHandler<>(
-                HelloFunction.class);
+                Hello.class);
         Greeting result = handler.handleRequest(new User("foo"), null);
         handler.close();
         assertThat(result.getMessage()).isEqualTo("Hello, foo!\n");
